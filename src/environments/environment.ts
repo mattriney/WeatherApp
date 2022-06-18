a0c1153d9d29f8e414adb79fec76497f45cc8c28
mattriney <61891660+mattriney@users.noreply.github.com>
2022-06-17 20:33:12 -0500

    api env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,15 @@
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '05beb5724emshfac4702ec46e2eap1f6798jsn11fa8380df39'
 };
 
 /*
